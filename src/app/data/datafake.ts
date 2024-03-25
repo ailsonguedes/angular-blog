f97d94e684a11eb5b97306259718597c6052250e
Ailson <ailsonsixseven@gmail.com>
2024-03-25 17:28:00 -0300

    feat(components): content created

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photoCover": "https://miro.medium.com/v2/resize:fit:1400/0*DTy8tlNHXGc0g4sB",
+    "title": "Novo Angular",
+    "description":"O Angular como uma forma de conhecer os frameworks para front-end na web. Sempre quando nos deparamos com a possibilidade de aprender uma nova tecnologia no mundo do desenvolvimento de software, a primeira pergunta que algumas pessoas costumam fazer é “qual tecnologia é a melhor para a finalidade x ou y” ou então, “qual dentre essas tecnologias é a menos custosa em relação a sua curva de aprendizado”.",
+  },
+
+  {
+    "id": "2",
+    "photoCover": "bbb",
+    "title": "Outro Velho Angular",
+    "description":"blablablskdsodksodksokablalblaalbla",
+  }
+]
